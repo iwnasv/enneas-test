dba0ae15a1f6db3ca3f6288d0555291819954677
iwnasv <34813745+iwnasv@users.noreply.github.com>
2023-05-25 19:19:26 +0300

    Update challenge.sh

--- challenge.sh
+++ challenge.sh
@@ -5,15 +5,15 @@ then
     echo "fatal: $dir is not a directory"
     exit 1
 fi
-mkdir 00 01 02
+mkdir "$dir"/00 "$dir"/01 "$dir"/02
 function sthThesiTou () {
     foo="$1" #edw mpainei 00, 01 h 02
-    if ! [ $foo =~ ^[0-9]+$ ]
+    if [ -z $foo ]
     then
-        echo "fatal: this function is only meant to work with integers"
+        echo "fatal: sorting function needs input"
         exit 1
     fi
-    find . -name "$foo*" -exec 'mv' {} "$foo" #vres ta arxeia pou ksekinane apo auto to integer p edwses k metafere ta sto antistoixo directory
+    find "$dir" -maxdepth 1 -type f -name "$foo*" -exec mv -t "$dir/$foo" {} \; #vres ta arxeia pou ksekinane apo auto to integer p edwses k metafere ta sto antistoixo directory
     #edw kalista mporei na mpei ena sha256sum prin k ena meta an 8es na eisai sigouros oti den xalases to arxeio apla profanws ksodeueis porous
     #to cp kai to mv den einai idiatera asfalh k gia pio euais8htes antigrafes xrhsimopoieis rsync
 }
